fc69024eff3f5d272ddefbef65dc09ac492e881c
shalom-lab <15121177287@163.com>
2022-04-23 11:12:48 +0800

    update

--- local/code/code-04-13-.R
+++ local/code/code-04-13-.R
@@ -9,9 +9,9 @@ rm(list=ls())
 # VARIABLE
 df.url<-readRDS('data/df.url.RDS')
 df.url<-df.url %>%
-  bind_rows(c(v.date='2022-04-20',
-            v.url.case='https://mp.weixin.qq.com/s/5LQeyprKrAgx__a9Ul037w',
-            v.url.location='https://mp.weixin.qq.com/s/8qCvsE578Ehz6UcWYRBfXw')) %>%
+  bind_rows(c(v.date='2022-04-21',
+            v.url.case='https://mp.weixin.qq.com/s/BTtYkDdU6t6OGF0a8kE3Zg',
+            v.url.location='https://mp.weixin.qq.com/s/qFvUyEB-R-GKP7vgKR-c3A')) %>%
   distinct(v.date,.keep_all = T) %>%
   arrange(v.date)
 saveRDS(df.url,'data/df.url.RDS')
@@ -53,8 +53,8 @@ df.asym.1<-data.frame(text=html.case %>% html_elements('p') %>% html_text()) %>%
          date=ymd(v.date))
 
 # fill group
-df.case.1$group<-mf.tag(c('isolation','screen','asym'),c(1,17,30),45)
-df.asym.1$group<-mf.tag(c('isolation','screen'),c(1,17),30)
+df.case.1$group<-mf.tag(c('isolation','screen','asym'),c(1,17,29),41)
+df.asym.1$group<-mf.tag(c('isolation','screen'),c(1,17),29)
 
 df.case.2 <-df.case.1 %>%
   select(date,district,group,n)
