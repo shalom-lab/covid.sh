3741338bf69dadb4cecf1539ce5ff24b46f911fe
shalom-lab <15121177287@163.com>
2022-06-09 10:11:43 +0800

    update

--- local/code/code-04-13-.R
+++ local/code/code-04-13-.R
@@ -9,14 +9,14 @@ rm(list=ls())
 # VARIABLE
 df.url<-readRDS('data/df.url.RDS')
 df.url<-df.url %>%
-  bind_rows(c(v.date='2022-06-07',
-            v.url.case='https://mp.weixin.qq.com/s/PBg0qj6H8W5cckemVmqdNQ',
-            v.url.location='https://mp.weixin.qq.com/s/W9tBwbc2tSNe7-zDWw_ViA')) %>%
+  bind_rows(c(v.date='2022-06-08',
+            v.url.case='https://mp.weixin.qq.com/s/L_Q9Qy0foNmtPqmn7Ta0mg',
+            v.url.location='https://mp.weixin.qq.com/s/OKxw0qxTtCy_utS5ilh6Lw')) %>%
   distinct(v.date,.keep_all = T) %>%
   arrange(v.date)
 saveRDS(df.url,'data/df.url.RDS')
 
-tem.df<-filter(df.url,v.date=='2022-06-07')
+tem.df<-filter(df.url,v.date=='2022-06-08')
 
 v.date<-pull(tem.df,v.date)
 v.url.case<-pull(tem.df,v.url.case)
@@ -59,8 +59,8 @@ df.asym.1<-data.frame(text=html.case %>% html_elements(tag) %>% html_text()) %>%
   filter(!is.na(district))
 
 # fill group
-df.case.1$group<-mf.tag(c('isolation','screen'),c(1,3),dim(df.case.1)[1])
-df.asym.1$group<-mf.tag(c('isolation','screen'),c(1,7),dim(df.asym.1)[1])
+df.case.1$group<-mf.tag(c('isolation'),c(1),dim(df.case.1)[1])
+df.asym.1$group<-mf.tag(c('isolation'),c(1),dim(df.asym.1)[1])
 
 df.case.2 <-df.case.1 %>%
   select(date,district,group,n)
